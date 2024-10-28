fb554829320cbf4eb3de1f2ce781f97d52d584e8
salihkaankoc <68756805+berkalimeral@users.noreply.github.com>
2024-10-28 16:55:50 +0300

    import go_router

--- lib/features/episode/view/episode_view.dart
+++ lib/features/episode/view/episode_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod_restapi/core/extensions/context_extension.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../core/widgets/error/error_screen.dart';
 import '../../../core/widgets/loading/loading_screen.dart';
